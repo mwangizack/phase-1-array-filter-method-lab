8736e127c1ec86452f52582c29ceeb96965392d0
Zacchaeus Ndirangu Mwangi <ndirangu.zmwangi@gmail.com>
2024-04-05 23:37:07 +0300

    Define filter functions for drivers

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(driversArray, searchItem){
+    const lowercaseSearchItem = searchItem.toLowerCase();
+   return driversArray.filter(element => element.toLowerCase() === lowercaseSearchItem);
+}
+
+function fuzzyMatch(driversArray, searchItem){
+    const searchItemCharacters = searchItem.split('').length;
+    // console.log(`Length: ${searchItemCharacters}`);
+    return driversArray.filter(element => element.slice(0, searchItemCharacters) === searchItem)
+}
+
+// const arr = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+// console.log(fuzzyMatch(arr, 'Sa'))
+
+function matchName(driversArray, searchItem){
+    return driversArray.filter(element => element.name === searchItem);
+}
